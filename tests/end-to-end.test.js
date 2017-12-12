0f6e7f1ee16e7dfdeb9a270083014e0c2de9d065
drico <dri9595@gmail.com>
2017-12-12 19:59:44 -0200

    added test to logical asserts

--- tests/end-to-end.test.js
+++ tests/end-to-end.test.js
@@ -3,3 +3,4 @@ import test from 'ava'
 
 test('simple hello world program', testProgram({ program: 'hello' }))
 test('arithmetic expressions', testProgram({ program: 'arithmetic-expression' }))
+test('logical asserts', testProgram({ program: 'logical-assert' }))
